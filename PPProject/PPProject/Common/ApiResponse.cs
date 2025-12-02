d567ef16cbce89a5336b91e83abcef092ccb0088
Minoritygames2 <117324974+Minoritygames2@users.noreply.github.com>
2025-12-02 16:42:08 +0900

    feat: ApiResponse 응답 추가

---
+++ PPProject/PPProject/Common/ApiResponse.cs
@@ -0,0 +1,38 @@
+﻿namespace PPProject.Common
+{
+    public class ApiResponse<T>
+    {
+        public enum ResultStatus
+        {
+            Success = 0,
+            Error   = 1001,
+            DBError = 1002,
+        }
+
+        public ResultStatus Status { get; private set; }
+        public T? Data { get; private set; }
+        public int ErrorCode { get; private set; }
+        public string? ErrorMessage { get; private set; }
+        public static ApiResponse<T> Success(T data)
+        {
+            return new ApiResponse<T>
+            {
+                Status = ResultStatus.Success,
+                Data = data,
+                ErrorCode = 0,
+                ErrorMessage = null
+            };
+        }
+
+        public static ApiResponse<T> Error(int ErrorCode, string errorMessage)
+        {
+            return new ApiResponse<T>
+            {
+                Status = ResultStatus.Error,
+                Data = default,
+                ErrorCode = ErrorCode,
+                ErrorMessage = errorMessage
+            };
+        }
+    }
+}
--- PPProject/PPProject/Controllers/WeatherForecastController.cs
+++ PPProject/PPProject/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using PPProject.Common;
 
 namespace PPProject.Controllers
 {
@@ -12,15 +13,16 @@ namespace PPProject.Controllers
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public ApiResponse<IEnumerable<WeatherForecast>> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+            return ApiResponse<IEnumerable<WeatherForecast>>.Success(result);
         }
     }
 }

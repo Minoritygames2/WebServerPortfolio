b61929285af56033c9a3c44707ef4a33f473193e
Minoritygames2 <117324974+Minoritygames2@users.noreply.github.com>
2025-12-12 18:19:14 +0900

    refactor: 게스트 로그인의 로직 baseLogin으로 이동

---
+++ PPProject/PPProject/Auth/Service/BaseLoginService.cs
@@ -0,0 +1,34 @@
+﻿using PPProject.Auth.DTO;
+using PPProject.Auth.Infrastructure;
+
+namespace PPProject.Auth.Service
+{
+    public abstract class BaseLoginService
+    {
+        protected readonly UserRepository _userRepository;
+        protected BaseLoginService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public virtual async Task<LoginResult> LoginAsync(int platformCode, string platformUserId)
+        {
+            //DB에 유저 정보가 있는지 확인하고, 존재한다면 유저 정보를 반환
+            var user = await _userRepository.GetByPlatformIdAsync(platformCode, platformUserId);
+            if (user != null)
+                return new LoginResult()
+                {
+                    UserId = user.uId,
+                    Status = user.Status
+                };
+
+            //유저를 생성
+            var newUser = await _userRepository.CreateAsync(platformCode, platformUserId);
+            return new LoginResult()
+            {
+                UserId = newUser.uId,
+                Status = newUser.Status
+            };
+        }
+    }
+}
--- PPProject/PPProject/Auth/Service/GuestLoginService.cs
+++ PPProject/PPProject/Auth/Service/GuestLoginService.cs
@@ -1,36 +1,12 @@
-﻿
-using PPProject.Auth.DTO;
-using PPProject.Auth.Infrastructure;
+﻿using PPProject.Auth.Infrastructure;
 
 namespace PPProject.Auth.Service
 {
-    public class GuestLoginService : ILoginService
+    public class GuestLoginService : BaseLoginService
     {
-        private readonly UserRepository _repository;
-
-        public GuestLoginService(UserRepository repository)
+        public GuestLoginService(UserRepository userRepository) : base(userRepository)
         {
-            _repository = repository;
-        }
-
-        public async Task<LoginResult> LoginAsync(int platformCode, string platformUserId)
-        {
-            //DB에 유저 정보가 있는지 확인하고, 존재한다면 유저 정보를 반환
-            var user = await _repository.GetByPlatformIdAsync(platformCode, platformUserId);
-            if (user != null)
-                return new LoginResult()
-                {
-                    UserId = user.uId,
-                    Status = user.Status
-                };
-
-            //유저를 생성
-            var newUser = await _repository.CreateAsync(platformCode, platformUserId);
-            return new LoginResult()
-            {
-                UserId = newUser.uId,
-                Status = newUser.Status
-            };
+            
         }
     }
 }

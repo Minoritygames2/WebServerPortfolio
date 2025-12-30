47fde9bbb5c6523e27608217997a347af2cfa681
Minoritygames2 <117324974+Minoritygames2@users.noreply.github.com>
2025-12-30 16:58:48 +0900

    feat: Log 기능 추가

---
+++ PPProject/PPProject/Middleware/Logger/ExceptionLoggingMiddleware.cs
@@ -0,0 +1,43 @@
+﻿namespace PPProject.Middleware.Logger
+{
+    public class ExceptionLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionLoggingMiddleware> _logger;
+
+        public ExceptionLoggingMiddleware(
+            RequestDelegate requestDelegate,
+            ILogger<ExceptionLoggingMiddleware> logger)
+        {
+            _next = requestDelegate;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(
+                       "{Method} {Path} {QueryString} | TraceId : {TraceId}",
+                       context.Request.Method,
+                       context.Request.Path,
+                       context.Request.QueryString.Value,
+                       context.TraceIdentifier
+                    );
+
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsJsonAsync(
+                    new
+                    {
+                        message = ex.Message
+                    });
+            }
+        }
+    }
+}
---
+++ PPProject/PPProject/Middleware/Logger/RequestLoggingMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using PPProject.Common;
+using System.Text;
+
+namespace PPProject.Middleware.Logger
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Request.EnableBuffering();
+
+            var requestBody = string.Empty;
+
+            if(context.Request.ContentLength > 0)
+            {
+                using var reader = new StreamReader(context.Request.Body, Encoding.UTF8, leaveOpen: true);
+                requestBody = await reader.ReadToEndAsync();
+                context.Request.Body.Position = 0;
+            }
+
+            context.Items[Define.PLAIN_REQ_BODY] = requestBody;
+
+            await _next(context);
+        }
+    }
+}
